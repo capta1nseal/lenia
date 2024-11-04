231a5caaf12987b6c3159d3bdda55bf9bb8da218
capta1nseal <aidan@lunesdale.community>
2024-11-04 20:11:47 +0200

    In-progress test commit.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include "application.hpp"
 
 #include <string>
-#include <exception>
 #include <iostream>
 
 int main (int argc, char *argv[])
@@ -10,13 +9,51 @@ int main (int argc, char *argv[])
     int radius;
     float timeFrequency;
 
-    bool useDefaultParams = true;
+    bool useDefaultParams;
+
+    bool validInputEntered = false;
+
+    for (validInputEntered == false) {
+        std::cout << "Use default paramaters, or enter manually? (1/2)\n";
+        std::string option;
+        std::cin >> option;
+
+        validInputEntered = true;
+
+        switch (option) {
+        case "1":
+            useDefaultParams = true;
+            break;
+        case "2":
+            useDefaultParams = false;
+            break;
+        default:
+            validInputEntered = false;
+            std::cout << "Invalid input. Please try again.\n";
+        }
+    }
 
     if (useDefaultParams) {
         width = 16u*30u;
         height = 9u*30u;
         radius = 12;
         timeFrequency = 3.0;
+    } else {
+        validInputEntered = false;
+        for (validInputEntered == false) {
+            std::cout << "Enter width:\n";
+            int option;
+            std::cin >> option;
+
+            validInputEntered = true;
+
+            if (option > 0) {
+                width = std::static_cast<unsigned int>(option);
+            } else {
+                validInputEntered = false;
+                std::cout << "Width must be positive and non-zero. Try again.\n";
+            }
+        }
     }
 
     std::cout << "Size: (" << width << "," << height << "), Kernel Radius: " << radius << ", Time Frequency: " << timeFrequency << "\n";
